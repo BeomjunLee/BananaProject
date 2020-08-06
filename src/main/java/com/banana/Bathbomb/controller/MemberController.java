e6d6fc6ac9cddb02e5ac72ec443b046409702813
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2020-08-06 22:04:01 +0900

    기능 틀 추가

--- src/main/java/com/banana/Bathbomb/controller/MemberController.java
+++ src/main/java/com/banana/Bathbomb/controller/MemberController.java
@@ -1,15 +1,39 @@
 package com.banana.Bathbomb.controller;
 
+import com.banana.Bathbomb.domain.Member;
 import com.banana.Bathbomb.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
 
-    @Autowired public MemberController(MemberService memberService){
-        this.memberService = memberService;
+    @GetMapping("")
+    public String home(){
+        return "";
     }
 
+    @GetMapping("/")
+        public String h(){
+            return "";
+        }
+
+
+    @PostMapping("/")
+    public String signUp(MemberForm form, HttpSession session){
+        String memberId = form.getId();
+        Member member = new Member();
+        member.setMemberId(memberId);
+        memberService.join(member);
+        //멤버 찾기
+        Member findMember = memberService.findMemberById(memberId);
+        session.setAttribute("sessionId", findMember.getMemberUid());
+        return "redirect:/homepage";
+    }
 }
--- src/main/java/com/banana/Bathbomb/controller/MemberForm.java
+++ src/main/java/com/banana/Bathbomb/controller/MemberForm.java
@@ -1,13 +1,13 @@
-package com.banana.Bathbomb.domain;
+package com.banana.Bathbomb.controller;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Member {
-    private int memberUid;           //회원 uid
-    private String memberId;              //회원 아이디
+public class MemberForm {
+    private int uid;           //회원 uid
+    private String id;              //회원 아이디
 //    private String pw;              //회원 아이디
 //    private String name;            //회원 이름
 //    private String phoneNum;        //회원 전화번호
@@ -15,5 +15,4 @@ public class Member {
 //    private int gender;             //회원 성별1이면 남자 0이면 여자
 //    private String regDate;         //회원 가입일자
 //    private String recentlyItem;   //최근 구독한 상품
-
 }
