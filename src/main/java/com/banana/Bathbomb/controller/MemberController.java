01d7fecaf9f33ffe2c8ffaadd3a0b932d2b8ad93
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2020-08-07 19:32:03 +0900

    오류사항 수정

--- src/main/java/com/banana/Bathbomb/controller/MemberController.java
+++ src/main/java/com/banana/Bathbomb/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.banana.Bathbomb.controller;
 import com.banana.Bathbomb.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/signUp")//회원가입 폼으로
+    public String goSignUpForm(){
+        return "/signUp";
+    }
 
 
 }
