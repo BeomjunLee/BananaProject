43e5481705c03eda723d35bc21397562c6a44bf4
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2020-08-08 18:43:55 +0900

    html uri변경

--- src/main/java/com/banana/Bathbomb/controller/SubscribeController.java
+++ src/main/java/com/banana/Bathbomb/controller/SubscribeController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SubscribeController {
     private final SubscribeService subscribeService;
 
-    @GetMapping("/mySubscribeList")//구독 내역으로
+    @GetMapping("/myPage/mySubscribeList")//구독 내역으로
     public String mySubscribeList(){
         return "/myPage/mySubscribeList";
     }
 
-    @GetMapping("/subscribe")//구독하기 버튼
+    @GetMapping("/subscribe/subscribe")//구독하기 버튼
     public String subscribe(){
         return "subscribe/subscribeType";
     }
 
-    @GetMapping("/subscribeResult")//구독 결제 정보
+    @GetMapping("/subscribe/subscribeResult")//구독 결제 정보
     public String subscribeResult(){
         return "/subscribe/subscribeResult";
     }
 
-    @GetMapping("/subscribeInfo")//구독 결제 입력
+    @GetMapping("/subscribe/subscribeInfo")//구독 결제 입력
     public String subscribeInfo(){
         return "/subscribe/subscribeInfo";
     }
